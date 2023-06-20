70824811f155e9167e082bead61c311383b91d5d
Adrian Freisinger <afreisinger@gmail.com>
2023-06-20 21:52:52 +0000

    add changes

--- common-setup/setup.sh
+++ common-setup/setup.sh
@@ -72,7 +72,7 @@ carefully_replace_gitconfig(){
 ln -sf "$SCRIPT_DIR"/profile "$HOME"/.profile
 ln -sf "$SCRIPT_DIR"/bashrc "$HOME"/.bashrc
 ln -sf "$SCRIPT_DIR"/gitignore_global "$HOME"/.gitignore_global
-ln -sf "$SCRIPT_DIR"/pystartup "$HOME"/.pystartup
+#ln -sf "$SCRIPT_DIR"/pystartup "$HOME"/.pystartup
 ln -sf "$SCRIPT_DIR"/tmux.conf "$HOME"/.tmux.conf
 carefully_replace_gitconfig
 
@@ -94,69 +94,69 @@ for file in "$SCRIPT_DIR"/bash.d/*; do
 done
 
 # Remove any existing symlink (or dir on mingw) - will fail if it is a dir
-rm -rf "$HOME"/.vim 2>/dev/null
-if [ ! -e "$HOME"/.vim ]; then  
-    ln -sf "$SCRIPT_DIR"/vim/dotvim "$HOME"/.vim
-fi
+# rm -rf "$HOME"/.vim 2>/dev/null
+# if [ ! -e "$HOME"/.vim ]; then  
+#     ln -sf "$SCRIPT_DIR"/vim/dotvim "$HOME"/.vim
+# fi
 
-ln -sf "$SCRIPT_DIR"/vim/vimrc "$HOME"/.vimrc
+# ln -sf "$SCRIPT_DIR"/vim/vimrc "$HOME"/.vimrc
 
-# Checks out the Vundle submodule
-git submodule update --init --recursive
+# # Checks out the Vundle submodule
+# git submodule update --init --recursive
 
-h2 "Installing all VIM plugins"
-info "(might take some time the first time ... )"
-vim +PlugInstall +qall
+# h2 "Installing all VIM plugins"
+# info "(might take some time the first time ... )"
+# vim +PlugInstall +qall
 
-h2 "Vim Fugitive setup"
-vim -X -u NONE -c "helptags ~/.vim/plugged/vim-fugitive/doc" -c q
+# h2 "Vim Fugitive setup"
+# vim -X -u NONE -c "helptags ~/.vim/plugged/vim-fugitive/doc" -c q
 
-# Needed for Typescript support in CoC and YCM using tsserver
-ts_cmd='npm install -g typescript'
-if which npm > /dev/null 2>&1 ; then
-    which tsc > /dev/null 2>&1 || bash -c "$ts_cmd"
-else
-    banner "Install NodeJS and run '$ts_cmd' to get TypeScript support in Vim"
-fi
+# # Needed for Typescript support in CoC and YCM using tsserver
+# ts_cmd='npm install -g typescript'
+# if which npm > /dev/null 2>&1 ; then
+#     which tsc > /dev/null 2>&1 || bash -c "$ts_cmd"
+# else
+#     banner "Install NodeJS and run '$ts_cmd' to get TypeScript support in Vim"
+# fi
 
-touch "$HOME"/.vimrc.local
+# touch "$HOME"/.vimrc.local
 
 # Install NeoVim config (we don't have to worry about XDG_CONFIG_HOME stuff
-[[ ! -e "$HOME"/.config ]] && mkdir "$HOME/.config"
-rm -rf ~/.config/nvim 
-ln -sf ~/.vim ~/.config/nvim
-ln -sf ~/.vimrc ~/.config/nvim/init.vim
+# [[ ! -e "$HOME"/.config ]] && mkdir "$HOME/.config"
+# rm -rf ~/.config/nvim 
+# ln -sf ~/.vim ~/.config/nvim
+# ln -sf ~/.vimrc ~/.config/nvim/init.vim
 
-# Install a better matcher for Ctrl-P
-if ! command_exists matcher; then 
-    if ! command_exists make; then 
-        banner "'make' is not installed. Rerun the setup after the per-machine setup completes)"
-    else
-        (cd matcher
-        make 
-        make install PREFIX=$HOME
-        cd ..) > /dev/null
-    fi
-fi
+# # Install a better matcher for Ctrl-P
+# if ! command_exists matcher; then 
+#     if ! command_exists make; then 
+#         banner "'make' is not installed. Rerun the setup after the per-machine setup completes)"
+#     else
+#         (cd matcher
+#         make 
+#         make install PREFIX=$HOME
+#         cd ..) > /dev/null
+#     fi
+# fi
 
 # Make a config file for ngrok, passing in the secret auth token
-[[ ! -e "$HOME"/.ngrok2 ]] && mkdir "$HOME/.ngrok2"
-NGROK_YML="$HOME/.ngrok2/ngrok.yml"
-rm "$NGROK_YML" 2> /dev/null || :;
-cat > "$NGROK_YML" << EOF
-###############################################################
-## WARNING: this file is auto-generated. See dotfiles repo   ##
-###############################################################
-authtoken: ${NGROK_AUTHTOKEN:-'fill-NGROK_AUTHTOKEN-in-in-~/.secret'} 
+# [[ ! -e "$HOME"/.ngrok2 ]] && mkdir "$HOME/.ngrok2"
+# NGROK_YML="$HOME/.ngrok2/ngrok.yml"
+# rm "$NGROK_YML" 2> /dev/null || :;
+# cat > "$NGROK_YML" << EOF
+# ###############################################################
+# ## WARNING: this file is auto-generated. See dotfiles repo   ##
+# ###############################################################
+# authtoken: ${NGROK_AUTHTOKEN:-'fill-NGROK_AUTHTOKEN-in-in-~/.secret'} 
 
-EOF
-cat $SCRIPT_DIR/ngrok.yml >> "$NGROK_YML"
+# EOF
+# cat $SCRIPT_DIR/ngrok.yml >> "$NGROK_YML"
 
-h2 "Copying psql config"
-ln -sf "$SCRIPT_DIR"/psqlrc $HOME/.psqlrc
+# h2 "Copying psql config"
+# ln -sf "$SCRIPT_DIR"/psqlrc $HOME/.psqlrc
 
 # Download to latest to home dir
-h2 "Fetching rupa/z (Jump Around)"
+h2 "Fetching Jump Around"
 curl -s https://raw.githubusercontent.com/rupa/z/master/z.sh -o ~/bin/z.sh
 
 h2 "Finished common setup"
--- utils/setup.sh
+++ utils/setup.sh
@@ -17,39 +17,39 @@ if ! command_exists millis ; then
     popd > /dev/null
 fi
 
-if ! command_exists signal-reset; then
-    h2 "Installing 'signal-reset'"
-    pushd signal-reset > /dev/null
-    make
-    cp signal-reset $HOME/bin/
-    popd > /dev/null
-fi
+# if ! command_exists signal-reset; then
+#     h2 "Installing 'signal-reset'"
+#     pushd signal-reset > /dev/null
+#     make
+#     cp signal-reset $HOME/bin/
+#     popd > /dev/null
+# fi
 
-# inotify-info
-# The better native version of my own script :D 
-if (! is_mac ) && (! is_wsl)  && (! command_exists inotify-info); then
-    h2 "Installing inotify-info"
-    pushd inotify-info/
-    make
-    cp _release/inotify-info ~/bin/
-    popd
-fi
+# # inotify-info
+# # The better native version of my own script :D 
+# if (! is_mac ) && (! is_wsl)  && (! command_exists inotify-info); then
+#     h2 "Installing inotify-info"
+#     pushd inotify-info/
+#     make
+#     cp _release/inotify-info ~/bin/
+#     popd
+# fi
 
-if (! command_exists fetch-todays-calendar); then
-    h2 "Install utility to fetch todays calendar"
-    WRAPPER="$HOME/bin/fetch-todays-calendar"
-    cd ./fetch-todays-calendar
-    npm i
-    cd ..
-    command cat > $WRAPPER <<__DELIM
-#!/usr/bin/env bash
-# vi: ft=bash
+# if (! command_exists fetch-todays-calendar); then
+#     h2 "Install utility to fetch todays calendar"
+#     WRAPPER="$HOME/bin/fetch-todays-calendar"
+#     cd ./fetch-todays-calendar
+#     npm i
+#     cd ..
+#     command cat > $WRAPPER <<__DELIM
+# #!/usr/bin/env bash
+# # vi: ft=bash
 
-cd "${SCRIPT_DIR}/fetch-todays-calendar"
-node app.js \$@
-__DELIM
-    chmod +x "$WRAPPER"
-fi
+# cd "${SCRIPT_DIR}/fetch-todays-calendar"
+# node app.js \$@
+# __DELIM
+#     chmod +x "$WRAPPER"
+# fi
 
 # symlink all scripts
 ln -sf "$SCRIPT_DIR/scripts/"* $HOME/bin/
