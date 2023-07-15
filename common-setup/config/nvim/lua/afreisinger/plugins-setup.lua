1b743da31081c25a4da542b5e31740ba4f47f3e4
Adrian Freisinger <afreisinger@gmail.com>
2023-07-15 12:15:14 -0300

    add files

--- common-setup/config/nvim/lua/afreisinger/plugins-setup.lua
+++ common-setup/config/nvim/lua/afreisinger/plugins-setup.lua
@@ -33,8 +33,9 @@ return packer.startup(function(use)
 
   use("nvim-lua/plenary.nvim") -- lua functions that many plugins use
 
-  --  use("bluz71/vim-nightfly-guicolors") -- preferred colorscheme
-  use("nordtheme/vim")
+  use("bluz71/vim-nightfly-guicolors") 
+  
+  use("nordtheme/vim") -- preferred colorscheme
 
   use("christoomey/vim-tmux-navigator") -- tmux & split window navigation
 
